8cb7baf0cde8adfefabcefe2d672c431d90c0421
Muhammad Abduh <muhammad.abduh081@gmail.com>
2025-02-05 05:50:01 +0700

    update dev

--- 0 - SP_IFRS_INITIAL_UPDATE.sql
+++ 0 - SP_IFRS_INITIAL_UPDATE.sql
@@ -1,7 +1,7 @@
 ---- DROP PROCEDURE SP_IFRS_INITIAL_UPDATE;
 
 CREATE OR REPLACE PROCEDURE SP_IFRS_INITIAL_UPDATE(
-    IN P_RUNID VARCHAR(5) DEFAULT 'S0000', 
+    IN P_RUNID VARCHAR(20) DEFAULT 'S_000_0000', 
     IN P_DOWNLOAD_DATE DATE DEFAULT NULL,
     IN P_PRC VARCHAR(1) DEFAULT 'S')
 LANGUAGE PLPGSQL 
@@ -60,13 +60,13 @@ BEGIN
     IF P_PRC = 'S' THEN 
         V_TABLEINSERT1 := 'IFRS_PD_RULES_CONFIG_' || P_RUNID || '';
         V_TABLEINSERT2 := 'IFRS_CCF_RULES_CONFIG_' || P_RUNID || '';
-        V_TABLEINSERT3 := 'IFRS_LGD_RULES_CONFIG_' || P_RUNID || '';
-        V_TABLEINSERT4 := 'IFRS_EAD_RULES_CONFIG_' || P_RUNID || '';
+        V_TABLEINSERT3 := 'IFRS_EAD_RULES_CONFIG_' || P_RUNID || '';
+        V_TABLEINSERT4 := 'IFRS_LGD_RULES_CONFIG_' || P_RUNID || '';
     ELSE 
         V_TABLEINSERT1 := 'IFRS_PD_RULES_CONFIG';
         V_TABLEINSERT2 := 'IFRS_CCF_RULES_CONFIG';
-        V_TABLEINSERT3 := 'IFRS_LGD_RULES_CONFIG';
-        V_TABLEINSERT4 := 'IFRS_EAD_RULES_CONFIG';
+        V_TABLEINSERT3 := 'IFRS_EAD_RULES_CONFIG';
+        V_TABLEINSERT4 := 'IFRS_LGD_RULES_CONFIG';
     END IF;
 
     IF P_DOWNLOAD_DATE IS NULL 
@@ -87,6 +87,15 @@ BEGIN
     -------- ====== VARIABLE ======
 
     -------- ====== PRE SIMULATION TABLE ======
+
+    V_STR_QUERY := '';
+    V_STR_QUERY := V_STR_QUERY || 'DELETE FROM IFRS_RUNNING_LOGS WHERE RUN_ID = ''' || P_RUNID || ''' AND DOWNLOAD_DATE = ''' || CAST(V_CURRDATE AS VARCHAR(10)) || '''::DATE ';
+    EXECUTE (V_STR_QUERY);
+
+    V_STR_QUERY := '';
+    V_STR_QUERY := V_STR_QUERY || 'INSERT INTO IFRS_RUNNING_LOGS (DOWNLOAD_DATE, RUN_ID, PROCESS_ID, PROCESS_DATE)
+    SELECT ''' || CAST(V_CURRDATE AS VARCHAR(10)) || '''::DATE, ''' || P_RUNID || ''', PG_BACKEND_PID(), CURRENT_DATE';
+    EXECUTE (V_STR_QUERY);
 
     IF P_PRC = 'S' THEN
         V_STR_QUERY := 'DROP TABLE IF EXISTS ' || V_TABLEINSERT1 || '';
@@ -109,7 +118,7 @@ BEGIN
         V_STR_QUERY := V_STR_QUERY || '';
         EXECUTE (V_STR_QUERY);
 
-        V_STR_QUERY := 'CREATE TABLE ' || V_TABLEINSERT3 || ' AS SELECT * FROM IFRS_LGD_RULES_CONFIG WHERE 0=1';
+        V_STR_QUERY := 'CREATE TABLE ' || V_TABLEINSERT3 || ' AS SELECT * FROM IFRS_EAD_RULES_CONFIG WHERE 0=1';
         V_STR_QUERY := V_STR_QUERY || '';
         EXECUTE (V_STR_QUERY);
 
@@ -117,20 +126,12 @@ BEGIN
         V_STR_QUERY := V_STR_QUERY || '';
         EXECUTE (V_STR_QUERY);
 
-        V_STR_QUERY := 'CREATE TABLE ' || V_TABLEINSERT4 || ' AS SELECT * FROM IFRS_EAD_RULES_CONFIG WHERE 0=1';
+        V_STR_QUERY := 'CREATE TABLE ' || V_TABLEINSERT4 || ' AS SELECT * FROM IFRS_LGD_RULES_CONFIG WHERE 0=1';
         V_STR_QUERY := V_STR_QUERY || '';
         EXECUTE (V_STR_QUERY);
         -------- ====== PRE SIMULATION TABLE ======
 
         -------- ====== BODY ======
-        V_STR_QUERY := '';
-        V_STR_QUERY := V_STR_QUERY || 'DELETE FROM IFRS_RUNNING_LOGS WHERE RUN_ID = ''' || P_RUNID || ''' AND DOWNLOAD_DATE = ''' || CAST(V_CURRDATE AS VARCHAR(10)) || '''::DATE ';
-        EXECUTE (V_STR_QUERY);
-
-        V_STR_QUERY := '';
-        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO IFRS_RUNNING_LOGS (DOWNLOAD_DATE, RUN_ID, PROCESS_ID, PROCESS_DATE)
-        SELECT ''' || CAST(V_CURRDATE AS VARCHAR(10)) || '''::DATE, ''' || P_RUNID || ''', PG_BACKEND_PID(), CURRENT_DATE';
-        EXECUTE (V_STR_QUERY);
         -------- ====== PD ======
         V_STR_QUERY := '';
         V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT1 || ' (
@@ -142,18 +143,18 @@ BEGIN
         historical_data, effective_end_date, CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete,
         created_by, created_date, created_host, default_ratio_by, CASE pd_method WHEN ''''MAA_CORP'''' THEN 0 WHEN ''''EXT'''' THEN 0 ELSE 1 END AS lag_1month_flag,
         ''''PENDING'''' AS running_status
-        FROM "PdConfiguration_Dev" ORDER BY pkid ASC'') 
+        FROM "PdConfiguration" ORDER BY pkid ASC'') 
         AS IFRS_PD_RULES_CONFIG_DATA(
         PKID BIGINT, TM_RULE_NAME VARCHAR(250), SEGMENTATION_ID BIGINT, PD_METHOD VARCHAR(50), CALC_METHOD VARCHAR(20), BUCKET_GROUP VARCHAR(30), EXPECTED_LIFE BIGINT, INCREMENT_PERIOD BIGINT,
         HISTORICAL_DATA BIGINT, CUT_OFF_DATE DATE, ACTIVE_FLAG BIGINT, IS_DELETE BIGINT, CREATEDBY VARCHAR(36), CREATEDDATE DATE,
         CREATEDHOST VARCHAR(30), DEFAULT_RATIO_BY VARCHAR(20), LAG_1MONTH_FLAG BIGINT, RUNNING_STATUS VARCHAR(20)
         )';
         EXECUTE (V_STR_QUERY);
-        -------- ====== PD ======
 
         GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
         V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
         V_RETURNROWS := 0;
+        -------- ====== PD ======
 
         -------- ====== CCF ======
         V_STR_QUERY := '';
@@ -165,22 +166,171 @@ BEGIN
         SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, ccf_name, CASE WHEN RIGHT(segment_code, 3)::INT > 100 THEN RIGHT(segment_code, 3) ELSE RIGHT(segment_code, 2) END AS segment_code, ccf_method, calculation_method, calculation_method_id, effective_end_date, expected_ccf,
         CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete, created_by, created_date, created_host,
         CASE calculation_method WHEN ''''Simple'''' THEN 0 ELSE 1 END AS lag_1month_flag, ''''PENDING'''' AS running_status
-        FROM "CcfConfiguration_Dev" ORDER BY pkid ASC'') 
+        FROM "CcfConfiguration" ORDER BY pkid ASC'') 
         AS IFRS_CCF_RULES_CONFIG_DATA(
         PKID BIGINT, CCF_RULE_NAME VARCHAR(250), SEGMENTATION_ID BIGINT, CALC_METHOD VARCHAR(20), AVERAGE_METHOD VARCHAR(20), DEFAULT_RULE_ID BIGINT, CUT_OFF_DATE DATE,
         CCF_OVERRIDE BIGINT, ACTIVE_FLAG BIGINT, IS_DELETE BIGINT, CREATEDBY VARCHAR(36), CREATEDDATE DATE,
         CREATEDHOST VARCHAR(30), LAG_1MONTH_FLAG BIGINT, RUNNING_STATUS VARCHAR(20)
         )';
         EXECUTE (V_STR_QUERY);
-        -------- ====== CCF ======
 
         GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
         V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
         V_RETURNROWS := 0;
+        -------- ====== CCF ======
+
+        -------- ====== EAD ======
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT3 || ' (
+        PKID, EAD_RULE_NAME, SEGMENTATION_ID, EAD_BALANCE, ACTIVE_FLAG, IS_DELETE
+        )
+        SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, syscode_ead_config
+        ,RIGHT(segment_code, 2) AS segment_code, balance_source
+        ,CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete 
+        FROM "EadConfiguration" ORDER BY pkid ASC'') 
+        AS IFRS_EAD_RULES_CONFIG_DATA (
+        PKID BIGINT, EAD_RULE_NAME VARCHAR(100)
+        ,SEGMENTATION_ID BIGINT, EAD_BALANCE VARCHAR(250)
+        ,ACTIVE_FLAG BIGINT, IS_DELETE BIGINT)';
+        EXECUTE (V_STR_QUERY);
+
+        GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
+        V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
+        V_RETURNROWS := 0;
+        -------- ====== EAD ======
+
+        -------- ====== LGD ======
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT4 || ' (
+        PKID, DEFAULT_RULE_ID, LGD_RULE_NAME, SEGMENTATION_ID, LGD_METHOD, LGW_HISTORICAL_DATA, WORKOUT_PERIOD
+        ,MIN_VALUE, MAX_VALUE,CUT_OFF_DATE, CALC_METHOD, ACTIVE_FLAG, IS_DELETE, CREATEDBY, CREATEDDATE,
+        CREATEDHOST, LAG_1MONTH_FLAG, RUNNING_STATUS
+        )
+        SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, RIGHT(default_criteria, 2) AS default_criteria, lgd_name, RIGHT(segment_code, 2) AS segment_code,lgd_method
+        ,historical_data, workout_period, lgd_min, lgd_max, effective_end_date, calculation_method, CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete,
+        created_by, created_date, created_host, CASE direct_cost WHEN ''''0'''' THEN 0 ELSE 1 END AS lag_1month_flag, ''''PENDING'''' AS running_status
+        FROM "LgdConfiguration" ORDER BY pkid ASC'') 
+        AS IFRS_LGD_RULES_CONFIG_DATA(
+        PKID BIGINT, DEFAULT_RULE_ID BIGINT, LGD_RULE_NAME VARCHAR(250), SEGMENTATION_ID BIGINT, LGD_METHOD VARCHAR(50), LGW_HISTORICAL_DATA BIGINT, WORKOUT_PERIOD BIGINT
+        ,MIN_VALUE BIGINT, MAX_VALUE BIGINT,CUT_OFF_DATE DATE, CALC_METHOD VARCHAR(250), ACTIVE_FLAG BIGINT, IS_DELETE BIGINT, CREATEDBY VARCHAR(36), CREATEDDATE DATE,
+        CREATEDHOST VARCHAR(30), LAG_1MONTH_FLAG BIGINT, RUNNING_STATUS VARCHAR(20)
+        )';
+        EXECUTE (V_STR_QUERY);
+
+        GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
+        V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
+        V_RETURNROWS := 0;
+        -------- ====== LGD ======
 
         RAISE NOTICE 'SP_IFRS_INITIAL_UPDATE | AFFECTED RECORD : %', V_RETURNROWS2;
     ELSE
-        RAISE NOTICE 'PUBLISH MODE!';
+        -------- ====== PD ======
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'TRUNCATE TABLE ' || V_TABLEINSERT1 || '';
+        EXECUTE (V_STR_QUERY);
+
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT1 || ' (
+        PKID, TM_RULE_NAME, SEGMENTATION_ID, PD_METHOD, CALC_METHOD, BUCKET_GROUP, EXPECTED_LIFE, INCREMENT_PERIOD,
+        HISTORICAL_DATA, CUT_OFF_DATE, ACTIVE_FLAG, IS_DELETE, CREATEDBY, CREATEDDATE,
+        CREATEDHOST, DEFAULT_RATIO_BY, LAG_1MONTH_FLAG, RUNNING_STATUS
+        )
+        SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, pd_name, RIGHT(segment_code, 2) AS segment_code, pd_method, calculation_method_id, bucket_code, expected_lifetime, windows_moving,
+        historical_data, effective_end_date, CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete,
+        created_by, created_date, created_host, default_ratio_by, CASE pd_method WHEN ''''MAA_CORP'''' THEN 0 WHEN ''''EXT'''' THEN 0 ELSE 1 END AS lag_1month_flag,
+        ''''PENDING'''' AS running_status
+        FROM "PdConfiguration" ORDER BY pkid ASC'') 
+        AS IFRS_PD_RULES_CONFIG_DATA(
+        PKID BIGINT, TM_RULE_NAME VARCHAR(250), SEGMENTATION_ID BIGINT, PD_METHOD VARCHAR(50), CALC_METHOD VARCHAR(20), BUCKET_GROUP VARCHAR(30), EXPECTED_LIFE BIGINT, INCREMENT_PERIOD BIGINT,
+        HISTORICAL_DATA BIGINT, CUT_OFF_DATE DATE, ACTIVE_FLAG BIGINT, IS_DELETE BIGINT, CREATEDBY VARCHAR(36), CREATEDDATE DATE,
+        CREATEDHOST VARCHAR(30), DEFAULT_RATIO_BY VARCHAR(20), LAG_1MONTH_FLAG BIGINT, RUNNING_STATUS VARCHAR(20)
+        )';
+        EXECUTE (V_STR_QUERY);
+
+        GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
+        V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
+        V_RETURNROWS := 0;
+        -------- ====== PD ======
+
+        -------- ====== CCF ======
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'TRUNCATE TABLE ' || V_TABLEINSERT2 || '';
+        EXECUTE (V_STR_QUERY);
+
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT2 || ' (
+        PKID, CCF_RULE_NAME, SEGMENTATION_ID, CALC_METHOD, AVERAGE_METHOD, DEFAULT_RULE_ID, CUT_OFF_DATE,
+        CCF_OVERRIDE, ACTIVE_FLAG, IS_DELETE, CREATEDBY, CREATEDDATE,
+        CREATEDHOST, LAG_1MONTH_FLAG, RUNNING_STATUS
+        )
+        SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, ccf_name, CASE WHEN RIGHT(segment_code, 3)::INT > 100 THEN RIGHT(segment_code, 3) ELSE RIGHT(segment_code, 2) END AS segment_code, ccf_method, calculation_method, calculation_method_id, effective_end_date, expected_ccf,
+        CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete, created_by, created_date, created_host,
+        CASE calculation_method WHEN ''''Simple'''' THEN 0 ELSE 1 END AS lag_1month_flag, ''''PENDING'''' AS running_status
+        FROM "CcfConfiguration" ORDER BY pkid ASC'') 
+        AS IFRS_CCF_RULES_CONFIG_DATA(
+        PKID BIGINT, CCF_RULE_NAME VARCHAR(250), SEGMENTATION_ID BIGINT, CALC_METHOD VARCHAR(20), AVERAGE_METHOD VARCHAR(20), DEFAULT_RULE_ID BIGINT, CUT_OFF_DATE DATE,
+        CCF_OVERRIDE BIGINT, ACTIVE_FLAG BIGINT, IS_DELETE BIGINT, CREATEDBY VARCHAR(36), CREATEDDATE DATE,
+        CREATEDHOST VARCHAR(30), LAG_1MONTH_FLAG BIGINT, RUNNING_STATUS VARCHAR(20)
+        )';
+        EXECUTE (V_STR_QUERY);
+
+        GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
+        V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
+        V_RETURNROWS := 0;
+        -------- ====== CCF ======
+
+        -------- ====== EAD ======
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'TRUNCATE TABLE ' || V_TABLEINSERT3 || '';
+        EXECUTE (V_STR_QUERY);
+
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT3 || ' (
+        PKID, EAD_RULE_NAME, SEGMENTATION_ID, EAD_BALANCE, ACTIVE_FLAG, IS_DELETE
+        )
+        SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, syscode_ead_config
+        ,RIGHT(segment_code, 2) AS segment_code, balance_source
+        ,CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete 
+        FROM "EadConfiguration" ORDER BY pkid ASC'') 
+        AS IFRS_EAD_RULES_CONFIG_DATA (
+        PKID BIGINT, EAD_RULE_NAME VARCHAR(100)
+        ,SEGMENTATION_ID BIGINT, EAD_BALANCE VARCHAR(250)
+        ,ACTIVE_FLAG BIGINT, IS_DELETE BIGINT)';
+        EXECUTE (V_STR_QUERY);
+
+        GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
+        V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
+        V_RETURNROWS := 0;
+        -------- ====== EAD ======
+
+        -------- ====== LGD ======
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'TRUNCATE TABLE ' || V_TABLEINSERT4 || '';
+        EXECUTE (V_STR_QUERY);
+
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT4 || ' (
+        PKID, DEFAULT_RULE_ID, LGD_RULE_NAME, SEGMENTATION_ID, LGD_METHOD, LGW_HISTORICAL_DATA, WORKOUT_PERIOD
+        ,MIN_VALUE, MAX_VALUE,CUT_OFF_DATE, CALC_METHOD, ACTIVE_FLAG, IS_DELETE, CREATEDBY, CREATEDDATE,
+        CREATEDHOST, LAG_1MONTH_FLAG, RUNNING_STATUS
+        )
+        SELECT * FROM dblink(''workflow_db_access'', ''SELECT pkid, RIGHT(default_criteria, 2) AS default_criteria, lgd_name, RIGHT(segment_code, 2) AS segment_code,lgd_method
+        ,historical_data, workout_period, lgd_min, lgd_max, effective_end_date, calculation_method, CASE WHEN is_active = TRUE THEN 1 ELSE 0 END AS is_active, 0 AS is_delete,
+        created_by, created_date, created_host, CASE direct_cost WHEN ''''0'''' THEN 0 ELSE 1 END AS lag_1month_flag, ''''PENDING'''' AS running_status
+        FROM "LgdConfiguration" ORDER BY pkid ASC'') 
+        AS IFRS_LGD_RULES_CONFIG_DATA(
+        PKID BIGINT, DEFAULT_RULE_ID BIGINT, LGD_RULE_NAME VARCHAR(250), SEGMENTATION_ID BIGINT, LGD_METHOD VARCHAR(50), LGW_HISTORICAL_DATA BIGINT, WORKOUT_PERIOD BIGINT
+        ,MIN_VALUE BIGINT, MAX_VALUE BIGINT,CUT_OFF_DATE DATE, CALC_METHOD VARCHAR(250), ACTIVE_FLAG BIGINT, IS_DELETE BIGINT, CREATEDBY VARCHAR(36), CREATEDDATE DATE,
+        CREATEDHOST VARCHAR(30), LAG_1MONTH_FLAG BIGINT, RUNNING_STATUS VARCHAR(20)
+        )';
+        EXECUTE (V_STR_QUERY);
+
+        GET DIAGNOSTICS V_RETURNROWS = ROW_COUNT;
+        V_RETURNROWS2 := V_RETURNROWS2 + V_RETURNROWS;
+        V_RETURNROWS := 0;
+        -------- ====== LGD ======
+
+        RAISE NOTICE 'SP_IFRS_INITIAL_UPDATE | AFFECTED RECORD : %', V_RETURNROWS2;
     END IF;
     
 END;
