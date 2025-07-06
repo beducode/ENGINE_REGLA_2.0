c4e5fb9cb29f1e72af795f369b85da213bf1cadd
ruleluludamara <rulelulu833@gmail.com>
2025-07-06 23:16:49 +0700

    upudate

---
+++ 2. AMORTIZATION/40 - SP_LBM_SYNC_PAYM_CORE.sql
@@ -0,0 +1,122 @@
+---- DROP PROCEDURE SP_LBM_SYNC_PAYM_CORE;
+
+CREATE OR REPLACE PROCEDURE SP_LBM_SYNC_PAYM_CORE(
+    IN P_RUNID VARCHAR(20) DEFAULT 'S_00000_0000',
+    IN P_DOWNLOAD_DATE DATE DEFAULT NULL,
+    IN P_PRC VARCHAR(1) DEFAULT 'S')
+LANGUAGE PLPGSQL AS $$
+DECLARE
+    ---- DATE
+    V_PREVDATE DATE;
+    V_CURRDATE DATE;
+
+    ---- QUERY   
+    V_STR_QUERY TEXT;
+
+    ---- TABLE LIST       
+    V_TABLEINSERT VARCHAR(100);
+    V_TABLEINSERT1 VARCHAR(100);
+    V_TABLEINSERT2 VARCHAR(100);
+    
+    ---- CONDITION
+    V_RETURNROWS INT;
+    V_RETURNROWS2 INT;
+    V_TABLEDEST VARCHAR(100);
+    V_COLUMNDEST VARCHAR(100);
+    V_SPNAME VARCHAR(100);
+    V_OPERATION VARCHAR(100);
+
+    ---- RESULT
+    V_QUERYS TEXT;
+
+    --- VARIABLE
+    V_SP_NAME VARCHAR(100);
+    STACK TEXT; 
+    FCESIG TEXT;
+BEGIN 
+    -------- ====== VARIABLE ======
+	GET DIAGNOSTICS STACK = PG_CONTEXT;
+	FCESIG := substring(STACK from 'function (.*?) line');
+	V_SP_NAME := UPPER(LEFT(fcesig::regprocedure::text, POSITION('(' in fcesig::regprocedure::text)-1));
+
+    IF COALESCE(P_PRC, NULL) IS NULL THEN
+        P_PRC := 'S';
+    END IF;
+
+    IF COALESCE(P_RUNID, NULL) IS NULL THEN
+        P_RUNID := 'S_00000_0000';
+    END IF;
+
+    IF P_PRC = 'S' THEN 
+        V_TABLEINSERT := 'IFRS_LBM_PAYM_CORE_SRC_' || P_RUNID || '';
+        V_TABLEINSERT1 := 'IFRS_IMA_AMORT_CURR_' || P_RUNID || '';
+        V_TABLEINSERT2 := 'IFRS_PAYM_CORE_SRC_' || P_RUNID || '';
+    ELSE 
+        V_TABLEINSERT := 'IFRS_LBM_PAYM_CORE_SRC';
+        V_TABLEINSERT1 := 'IFRS_IMA_AMORT_CURR';
+        V_TABLEINSERT2 := 'IFRS_PAYM_CORE_SRC';
+    END IF;
+    
+    IF P_DOWNLOAD_DATE IS NULL 
+    THEN
+        SELECT
+            CURRDATE, PREVDATE INTO V_CURRDATE, V_PREVDATE
+        FROM
+            IFRS_PRC_DATE;
+    ELSE        
+        V_CURRDATE := P_DOWNLOAD_DATE;
+        V_PREVDATE := V_CURRDATE - INTERVAL '1 DAY';
+    END IF;
+    
+    V_RETURNROWS2 := 0;
+    -------- ====== VARIABLE ======
+
+    -------- ====== PRE SIMULATION TABLE ======
+    IF P_PRC = 'S' THEN
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'DROP TABLE IF EXISTS ' || V_TABLEINSERT || ' ';
+        EXECUTE (V_STR_QUERY);
+
+        V_STR_QUERY := '';
+        V_STR_QUERY := V_STR_QUERY || 'CREATE TABLE ' || V_TABLEINSERT || ' AS SELECT * FROM IFRS_TRANSACTION_PARAM WHERE 1=0 ';
+        EXECUTE (V_STR_QUERY);
+    END IF;
+    -------- ====== PRE SIMULATION TABLE ======
+
+    -------- ====== BODY ======
+    CALL SP_IFRS_LOG_AMORT(V_CURRDATE, 'START', V_SP_NAME, '');
+
+    V_STR_QUERY := '';
+    V_STR_QUERY := V_STR_QUERY || 'TRUNCATE TABLE ' || V_TABLEINSERT || ' ';
+    EXECUTE (V_STR_QUERY);
+    
+    V_STR_QUERY := '';
+    V_STR_QUERY := V_STR_QUERY || 'INSERT INTO ' || V_TABLEINSERT || ' 
+        SELECT B.* 
+        FROM (
+            SELECT * 
+            FROM ' || V_TABLEINSERT1 || ' 
+            WHERE (STAFF_LOAN_FLAG = ''Y'' 
+            OR BELOW_MARKET_FLAG = 1)) A
+        LEFT JOIN ' || V_TABLEINSERT2 || ' B ON A.MASTERID = B.MASTERID
+        ';
+    EXECUTE (V_STR_QUERY);
+    ---------- ====== BODY ======
+
+    -------- ====== LOG ======
+    V_TABLEDEST = V_TABLEINSERT;
+    V_COLUMNDEST = '-';
+    V_SPNAME = 'SP_LBM_SYNC_PAYM_CORE';
+    V_OPERATION = 'INSERT';
+    
+    CALL SP_IFRS_EXEC_AND_LOG(V_CURRDATE, V_TABLEDEST, V_COLUMNDEST, V_SPNAME, V_OPERATION, V_RETURNROWS2, P_RUNID);
+    -------- ====== LOG ======
+
+    -------- ====== RESULT ======
+    V_QUERYS = 'SELECT * FROM ' || V_TABLEINSERT || '';
+    CALL SP_IFRS_RESULT_PREV(V_CURRDATE, V_QUERYS, V_SPNAME, V_RETURNROWS2, P_RUNID);
+    -------- ====== RESULT ======
+
+END;
+
+$$;
