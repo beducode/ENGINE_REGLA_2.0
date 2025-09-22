1b30fcdac81a48c4b3c1f73a4d8c2e1b886a88f7
Muhammad Abduh <muhammad.abduh081@gmail.com>
2025-09-22 09:52:05 +0700

    update archiving process

---
+++ DEV_SCRIPT/[SCRIPT] INSERT DATA DBLINK CONNECTION.sql
@@ -0,0 +1,17 @@
+/*
+
+-- CREATE EXTENSION pgcrypto;
+
+INSERT INTO REGLA_DB_CONNECTION (DBCONN_PKID, DBHOST, DBNAME, DBUSER, DBPASS, DBPORT) VALUES (2
+,pgp_sym_encrypt('alloy.dev.regla.cloud','QymZvg/vd4HtrC+L0TLsGhlfunLM1n7Wbim11J1Dwq+3Qqj6rprAjZmgEm+s7qSAeIkx7aJgHb57FGnpav9xzDlvYoJA9b24pYNAFdKQGtCBOr3udKEZWt9O4vPt9SJ2qSd1gzhUn/PdngF5/qAjv/gLtt0TQYU4cqn1bM3pDcQ5Hv4gT/l4WJrOqoPbPRzHwNpPCG/O0A1qSvOS31njfQ==')
+,pgp_sym_encrypt('IFRS9','QymZvg/vd4HtrC+L0TLsGhlfunLM1n7Wbim11J1Dwq+3Qqj6rprAjZmgEm+s7qSAeIkx7aJgHb57FGnpav9xzDlvYoJA9b24pYNAFdKQGtCBOr3udKEZWt9O4vPt9SJ2qSd1gzhUn/PdngF5/qAjv/gLtt0TQYU4cqn1bM3pDcQ5Hv4gT/l4WJrOqoPbPRzHwNpPCG/O0A1qSvOS31njfQ==')
+,pgp_sym_encrypt('reglaalloy01','QymZvg/vd4HtrC+L0TLsGhlfunLM1n7Wbim11J1Dwq+3Qqj6rprAjZmgEm+s7qSAeIkx7aJgHb57FGnpav9xzDlvYoJA9b24pYNAFdKQGtCBOr3udKEZWt9O4vPt9SJ2qSd1gzhUn/PdngF5/qAjv/gLtt0TQYU4cqn1bM3pDcQ5Hv4gT/l4WJrOqoPbPRzHwNpPCG/O0A1qSvOS31njfQ==')
+,pgp_sym_encrypt('ynJlX2nPkXCCrXRdP70k0L7','QymZvg/vd4HtrC+L0TLsGhlfunLM1n7Wbim11J1Dwq+3Qqj6rprAjZmgEm+s7qSAeIkx7aJgHb57FGnpav9xzDlvYoJA9b24pYNAFdKQGtCBOr3udKEZWt9O4vPt9SJ2qSd1gzhUn/PdngF5/qAjv/gLtt0TQYU4cqn1bM3pDcQ5Hv4gT/l4WJrOqoPbPRzHwNpPCG/O0A1qSvOS31njfQ==')
+,8433)
+
+
+-- SELECT * FROM REGLA_DB_CONNECTION
+
+-- SELECT pgp_sym_decrypt(dbpass, 'QymZvg/vd4HtrC+L0TLsGhlfunLM1n7Wbim11J1Dwq+3Qqj6rprAjZmgEm+s7qSAeIkx7aJgHb57FGnpav9xzDlvYoJA9b24pYNAFdKQGtCBOr3udKEZWt9O4vPt9SJ2qSd1gzhUn/PdngF5/qAjv/gLtt0TQYU4cqn1bM3pDcQ5Hv4gT/l4WJrOqoPbPRzHwNpPCG/O0A1qSvOS31njfQ==')
+-- FROM REGLA_DB_CONNECTION
+*/
